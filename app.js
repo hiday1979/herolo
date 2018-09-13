1d46424dd9845472d14d843f239e6253b4dbd4cb
WigiTech <accounts@wigitech.com>
2018-09-13 18:58:17 +0300

    updates to deploy to heroku

---
+++ app.js
@@ -0,0 +1,20 @@
+//Install express server
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const api = require('./server/routes/api');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(path.join(__dirname ,'dist')));
+
+app.get('*', (req,res)=> {
+
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+const port = process.env.PORT || 8080;
+app.set('port', port);
+
+const server = http.createServer(app);
+// Start the app by listening on the default Heroku port
+server.listen(port, () => console.log('runinig'));
