b808ace1a2e3e9c2fd63575b2bcbb8a1429d721c
WigiTech <accounts@wigitech.com>
2018-09-16 12:29:59 +0300

    adding server.js file

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 //Install express server
 const express = require('express');
-const http = require('http');
 const path = require('path');
-const api = require('./server/routes/api');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname + 'dist')));
-
+app.use(express.static(__dirname + 'dist'));
+app.listen(process.env.PORT || 8080);
 app.get('/*',function (req,res) {
-res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-const port = process.env.PORT || 8080;
-app.set('port', port);
 
-const server = http.createServer(app);
-// Start the app by listening on the default Heroku port
-server.listen(port, () => console.log('runinig'));
